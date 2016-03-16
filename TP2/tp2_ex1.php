eb6a7a63526baed11d5fbe9c76b68a0147b33c4f
21504037 <21504037@etu.unicaen.fr>
2016-03-16 15:51:51 +0100

    Plein de fichiers

--- TP2/tp2_ex1.php
+++ TP2/tp2_ex1.php
@@ -2,9 +2,9 @@
     include("tp2_function.php");
     getHeader(False,"TP2");
 ?>
-        <form methode="post" action="">
+        <form methode="post" action="<?php echo $_SERVER['PHP_SELF']?>>
             <label for="nom">Saisissez votre prénom :</label>
-            <input type="text" name="nom" id="nom">
+            <input type="text" name="nom" id="nom" value="<?php echo $nom?>">
         </form>
 <?php
     $nom="";
--- TP2/tp2_ex1Validation.php
+++ TP2/tp2_ex1Validation.php
@@ -1,5 +1,6 @@
 <?php
     include("tp2_function.php");
+    include("ex1");
     getHeader(False,"TP2");
     echo $nom
 ?>
