f95fdf52066dc969558f91825e43a2675ac2f372
21504037 <21504037@etu.unicaen.fr>
2016-03-16 14:45:52 +0100

    fonction du tp2

---
+++ function_tp2.php
@@ -0,0 +1,5 @@
+<?php
+    #Permet  d'insérer l'en-tête HTLM5 d'une page
+    function getHeader($startSession,$title){
+        echo "<head><meta charset=\"UTF-8\"><title>$title</title><head>";
+            }
