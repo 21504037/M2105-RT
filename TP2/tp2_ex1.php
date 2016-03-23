26d9e53a412e83d40afbcfb1996686e9ba3e2aac
21504037 <21504037@etu.unicaen.fr>
2016-03-23 14:13:36 +0100

    TP2

--- TP2/tp2_ex1.php
+++ TP2/tp2_ex1.php
@@ -1,29 +1,34 @@
 <?php
     include("tp2_function.php");
     getHeader("coucou","TP2");
+
+    if(array_key_exists("nom",$_COOKIE)){
+        header("location: ex2_recoAuto.php");
+    }
+    if(array_key_exists("nom",$_POST)) {
+        $_SESSION["user"] = $_POST["nom"];
+    }
+    if (array_key_exists("ckRemember",$_POST)){
+        setcookie("nom",$_POST["nom"],time()+5);
+    }
 ?>
     <div class="container">
-            <form class="form-inline" method="post" action="ex1Validation.php">
-                    <fieldset>
-                            <legend>Connexion</legend>
-                            <div class="alert alert-info">Saisir votre nom et valider.</div>
-                            <div class="form-group">
-                                   <input placeholder="Nom..." class="form-control" type="text" name="nom">
-                                    <input class="btn btn-primary" type="submit" value="Valider">
-                            </div>
-                        </fieldset>
 
-            </form>
+        <form class="form-inline" method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
+            <fieldset>
+                <legend>Connexion</legend>
+                <div class="form-group">
+                    <label for="nom">Nom :</label>
+                    <input placeholder="Nom..." class="form-control" type="text" name="nom">
+                    <input class="btn btn-primary" type="submit" value="Valider">
+                    <hr/>
+                    <input type="checkbox" name="ckremember"> Mémoriser mes inofrmations de connexion
+                </div>
+            </fieldset>
+
+        </form>
     </div>
 
 <?php
-
-    if(array_key_exists("nom",$_POST)) {
-        $_SESSION["user"]=$_POST["nom"];
-        if (array_key_exists("ckRemember",$_POST)){
-          setcookie("nom",$_POST["nom"],time()+5*60);
-        }
-    }
-
     getFooter();
 ?>
--- TP2/tp2_ex1Validation.php
+++ TP2/tp2_ex1Validation.php
@@ -5,7 +5,7 @@
 
     echo $nom;
 
-    getFooter;
+    getFooter();
 ?>
 
 
